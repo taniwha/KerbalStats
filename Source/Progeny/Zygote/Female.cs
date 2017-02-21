a0b3a954474715f83e22442f0f3e6f98bc82c3a9
Bill Currie <bill@taniwha.org>
2017-02-21 12:22:48 +0900

    Move the female FSM into Female.
    In the end, having it separate caused more trouble than in saved.

--- Source/Progeny/Zygote/Female.cs
+++ Source/Progeny/Zygote/Female.cs
@@ -32,7 +32,7 @@ namespace KerbalStats.Progeny.Zygotes {
 		Gamete gamete;
 		Cycle cycle;
 
-		FemaleFSM fsm;
+		KerbalFSM fsm;
 
 		public bool isInterested ()
 		{
@@ -107,7 +107,7 @@ namespace KerbalStats.Progeny.Zygotes {
 		void initialize ()
 		{
 			lastUpdate = Planetarium.GetUniversalTime ();
-			fsm = new FemaleFSM (this);
+			CreateStateMachine ();
 
 			interest = new Interest (genes);
 			gamete = new Gamete (genes, true, bioClock);
@@ -176,6 +176,108 @@ namespace KerbalStats.Progeny.Zygotes {
 		public int CompareTo (Female other)
 		{
 			return name.CompareTo (other.name);
+		}
+
+
+		// State machine
+
+		KFSMState state_fertile;
+		KFSMState state_pregnant;
+		KFSMState state_discovered;
+		KFSMState state_resting;
+		KFSMState state_dead;
+
+		KFSMEvent event_conceive;
+		KFSMEvent event_discover;
+		KFSMEvent event_birthe;
+		KFSMEvent event_rested;
+
+		bool check_conceive (KFSMState st)
+		{
+			if (location.isWatched ()) {
+				return false;
+			}
+			if (!isInterested ()) {
+				return false;
+			}
+			var mate = SelectMate (location.Males ());
+			return mate != null ? Mate (mate) : false;
+		}
+
+		bool check_discover (KFSMState st)
+		{
+			return DiscoverPregnancy ();
+		}
+
+		void report_pregnancy (KFSMState prevState)
+		{
+			if (prevState == null || prevState == state_discovered) {
+				// called from StartFSM (or redundantly?) so already fired
+				return;
+			}
+			//Debug.LogFormat ("report_pregnancy: {0}", id);
+			ProgenyScenario.current.ReportPregnancy (this);
+		}
+
+		bool check_birthe (KFSMState st)
+		{
+			return false;
+		}
+
+		bool check_rested (KFSMState st)
+		{
+			return false;
+		}
+
+		void CreateStates ()
+		{
+			state_fertile = new KFSMState ("Fertile");
+
+			state_pregnant = new KFSMState ("Pregnant");
+
+			state_discovered = new KFSMState ("Discovered");
+			state_discovered.OnEnter = report_pregnancy;
+
+			state_resting = new KFSMState ("Resting");
+
+			state_dead = new KFSMState ("Dead");
+
+			fsm.AddState (state_fertile);
+			fsm.AddState (state_pregnant);
+			fsm.AddState (state_discovered);
+			fsm.AddState (state_resting);
+			fsm.AddState (state_dead);
+		}
+
+		void CreateEvents ()
+		{
+			event_conceive = new KFSMEvent ("Conceive");
+			event_conceive.GoToStateOnEvent = state_pregnant;
+			event_conceive.OnCheckCondition = check_conceive;
+
+			event_discover = new KFSMEvent ("Discover");
+			event_discover.GoToStateOnEvent = state_discovered;
+			event_discover.OnCheckCondition = check_discover;
+
+			event_birthe = new KFSMEvent ("Birthe");
+			event_birthe.GoToStateOnEvent = state_resting;
+			event_birthe.OnCheckCondition = check_birthe;
+
+			event_rested = new KFSMEvent ("Rested");
+			event_rested.GoToStateOnEvent = state_fertile;
+			event_rested.OnCheckCondition = check_rested;
+
+			fsm.AddEvent (event_conceive, new KFSMState [] { state_fertile, });
+			fsm.AddEvent (event_birthe, new KFSMState [] { state_pregnant, state_discovered});
+			fsm.AddEvent (event_discover, new KFSMState [] { state_pregnant, });
+			fsm.AddEvent (event_rested, new KFSMState [] { state_resting, });
+		}
+
+		void CreateStateMachine ()
+		{
+			fsm = new KerbalFSM ();
+			CreateStates ();
+			CreateEvents ();
 		}
 	}
 }
