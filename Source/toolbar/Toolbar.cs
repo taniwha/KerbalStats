e26378b60e6d03bc642c49afe9d58bb5f3ecc313
Bill Currie <bill@taniwha.org>
2017-08-08 23:07:32 +0900

    Add an applauncher button.
    Currently always enabled, but the guts are there.

--- Source/toolbar/Toolbar.cs
+++ Source/toolbar/Toolbar.cs
@@ -20,8 +20,63 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
+using KSP.UI.Screens;
+
 namespace KerbalStats {
 	using Toolbar;
+
+	[KSPAddon (KSPAddon.Startup.MainMenu, true)]
+	public class KSAppButton : MonoBehaviour
+	{
+		const ApplicationLauncher.AppScenes buttonScenes = ApplicationLauncher.AppScenes.SPACECENTER | ApplicationLauncher.AppScenes.FLIGHT | ApplicationLauncher.AppScenes.VAB | ApplicationLauncher.AppScenes.SPH;
+		private static ApplicationLauncherButton button = null;
+
+		public static Callback Toggle = delegate {};
+
+		static bool buttonVisible
+		{
+			get {
+				//if (ToolbarManager.Instance == null) {
+				//	return true;
+				//}
+				//return false;
+				return true;
+			}
+		}
+
+		public static void UpdateVisibility ()
+		{
+			if (button != null) {
+				button.VisibleInScenes = buttonVisible ? buttonScenes : 0;
+			}
+		}
+
+		void onToggle ()
+		{
+			//Toggle ();
+			Progeny.DebugWindow.ToggleGUI ();
+		}
+
+		void Start ()
+		{
+			GameObject.DontDestroyOnLoad(this);
+			GameEvents.onGUIApplicationLauncherReady.Add(OnGUIAppLauncherReady);
+		}
+
+		void OnDestroy ()
+		{
+			GameEvents.onGUIApplicationLauncherReady.Remove(OnGUIAppLauncherReady);
+		}
+
+		void OnGUIAppLauncherReady ()
+		{
+			if (ApplicationLauncher.Ready && button == null) {
+				var tex = GameDatabase.Instance.GetTexture("KerbalStats/Textures/progeny_icon_button", false);
+				button = ApplicationLauncher.Instance.AddModApplication(onToggle, onToggle, null, null, null, null, buttonScenes, tex);
+				UpdateVisibility ();
+			}
+		}
+	}
 
 	[KSPAddon (KSPAddon.Startup.EveryScene, false)]
 	public class KSToolbar_ProgenyDebug : MonoBehaviour
